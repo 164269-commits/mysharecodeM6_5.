f499b5bd7963c36194cf3110872f325ee8f15389
164269-commits <164269@wsk.ac.th>
2026-06-16 04:17:39 +0000

    first01.py 6, U

---
+++ first01.py
@@ -0,0 +1,6 @@
+print("โปรแกรมคำนวณหาเพื้นที่ของสามเหลี่ยม")
+h-float(input("enter hight:"))
+b-float(input("enter base:"))
+area=0.5*h*
+print("พื้นที่สามเหลี่ยมเท่ากับ",area)
+print(จัดทำโดย นาย สกลวรรธน์ บัวย้อย เลขที่ 10)
